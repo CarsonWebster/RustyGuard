18211174911994eb8e9baaa07c2d3eecf5aa78bf
Carson Webster <CarsonWebster@hotmail.com>
2024-03-05 17:27:50 -0800

    Creating message channels between front and back

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,50 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod echo_packet_sniffer;
+use echo_packet_sniffer::PacketSniffer;
+use std::sync::Mutex;
+use tauri::{State, Window};
+
+struct AppState {
+    packet_sniffer: Mutex<PacketSniffer>,
+}
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello {} from Rust!", name)
+}
+
+#[tauri::command]
+fn get_network_interfaces() -> Vec<String> {
+    pnet::datalink::interfaces()
+        .into_iter()
+        .map(|interface| interface.name)
+        .collect()
+}
+
+#[tauri::command]
+async fn start_packet_sniffing(
+    interface_name: String,
+    window: Window,
+    state: State<'_, AppState>,
+) -> Result<(), String> {
+    let mut sniffer = state
+        .packet_sniffer
+        .lock()
+        .map_err(|_| "Lock failed on start sniffing")?;
+    sniffer.start(interface_name, window);
+    Ok(())
+}
+
+#[tauri::command]
+async fn stop_packet_sniffing(state: State<'_, AppState>) -> Result<(), String> {
+    let mut sniffer = state
+        .packet_sniffer
+        .lock()
+        .map_err(|_| "Lock failed on stop sniffing")?;
+    sniffer.stop();
+    Ok(())
 }
 
 fn main() {
@@ -18,7 +59,15 @@ fn main() {
             }
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(AppState {
+            packet_sniffer: Mutex::new(PacketSniffer::new()),
+        })
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            get_network_interfaces,
+            start_packet_sniffing,
+            stop_packet_sniffing
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ import { skeleton } from '@skeletonlabs/tw-plugin';
 import forms from '@tailwindcss/forms';
 
 export default {
-	darkMode: 'class',
+	// darkMode: 'class',
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
 		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
